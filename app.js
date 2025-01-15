a3f5a4cf35f702b65b76216985af95c5e3b6d0d6
EvanMworia <evannji99@gmail.com>
2025-01-15 16:36:05 +0300

    Stashed Update-Branch Progress

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let movies= [{
-    "id": 1,
-    "title": "The Shawshank Redemption",
-    "year": 1994,
-    "rating": 9.3,
-    "image": "https://www.movieposters.com/cdn/shop/files/shawshank_eb84716b-efa9-44dc-a19d-c17924a3f7df_480x.progressive.jpg?v=1709821984",
-    "description":"The Shawshank Redemption is a 1994 film directed by Frank Darabont, based on Stephen King's novella Rita Hayworth and Shawshank Redemption. The story follows Andy Dufresne, a banker who is wrongfully convicted of murdering his wife and her lover. Sentenced to two consecutive life terms at Shawshank Prison, Andy befriends fellow inmate Ellis Red Redding."
-}]
-
-  
-const movieForm = document.getElementById('movie-form');
+let movies = [
+	{
+		id: 1,
+		title: 'The Shawshank Redemption',
+		year: 1994,
+		rating: 9.3,
+		image: 'https://www.movieposters.com/cdn/shop/files/shawshank_eb84716b-efa9-44dc-a19d-c17924a3f7df_480x.progressive.jpg?v=1709821984',
+		description:
+			"The Shawshank Redemption is a 1994 film directed by Frank Darabont, based on Stephen King's novella Rita Hayworth and Shawshank Redemption. The story follows Andy Dufresne, a banker who is wrongfully convicted of murdering his wife and her lover. Sentenced to two consecutive life terms at Shawshank Prison, Andy befriends fellow inmate Ellis Red Redding.",
+	},
+];
